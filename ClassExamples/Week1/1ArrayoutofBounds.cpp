51a031e5535262b24431ff82374ce71b9f97f3ec
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-23 19:20:31 -0500

    Update 1ArrayoutofBounds.cpp

--- ClassExamples/Week1/1ArrayoutofBounds.cpp
+++ ClassExamples/Week1/1ArrayoutofBounds.cpp
@@ -1,11 +1,11 @@
 // 19fall CS341 Week1 
-// C++ does not bounds checking for fixed-length arrarys
+// C++ does not bounds checking for fixed-length array
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	int array[5] { 0, 1, 2, 3, 4 };// declare an arrary of five numbers & assign values to each								 
+	int array[5] { 0, 1, 2, 3, 4 }; // declare an array of five numbers & assign values to each								 
 	int i, j;
         
 	// display original array
@@ -14,9 +14,9 @@ int main() {
 	cout << endl << endl;
 	
 	// add five locations on either sides of the original array
-	for (i = -5; i < 10; i++)		 
-		cout << array[i] << " ";	 
-	cout << endl;	
+	for (j = -5; j < 10; j++)		 
+		cout << array[j] << " ";	 
+	cout << endl; // Q.E.D
 	
 	return 0;
 }
