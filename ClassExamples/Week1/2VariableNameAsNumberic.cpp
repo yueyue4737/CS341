b82624ab9dc4023337366af94dd5406dab7e0ba6
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-23 19:25:39 -0500

    Update 2VariableNameAsNumberic.cpp

--- ClassExamples/Week1/2VariableNameAsNumberic.cpp
+++ ClassExamples/Week1/2VariableNameAsNumberic.cpp
@@ -1,16 +1,19 @@
-//  
+// Location
+//  64-bit systems use size_t instead of int
 
 #include <iostream>
 using namespace std;
 
 int main() {
+	// display location of arrary elements and its contents
 	int x = 10;
 	cout << "Location of x is at " << &x << " and contains " << x << endl;
 	cout << "Location of x+1 is at " << &x+1 << " and contains " << *(&x + 1) 
 		<< endl;
 
 	int array[5] { 0, 1, 2, 3, 4 };   
-									 
+	
+	// display location of a single variable
 	cout << "Location of first element of array is at " << array << endl;
 
 	int i, j;
@@ -18,8 +21,8 @@ int main() {
 	for (i = 0; i < 5; i++)			 
 		cout << array[i] << " ";
 	cout << endl;
-	for (i = -10; i < 10; i++)		 
-		cout << array[i] << " ";	 
+	for (j = -10; j < 10; j++)		 
+		cout << array[j] << " ";	 
 	cout << endl;					 
 	return 0;
 }
