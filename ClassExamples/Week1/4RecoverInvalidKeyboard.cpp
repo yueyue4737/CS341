6ef60da813cfba182e8daad4d989bb046917e152
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-23 19:43:03 -0500

    Update 4RecoverInvalidKeyboard.cpp

--- ClassExamples/Week1/4RecoverInvalidKeyboard.cpp
+++ ClassExamples/Week1/4RecoverInvalidKeyboard.cpp
@@ -1,30 +1,36 @@
-// 
+// recover from bad input numeric data
+// calculate sum, count and average
 
-#include <iostream>
-using namespace std;
+#include <iostream> // include library
+using namespace std; // using statement
+
+// global area
 
 int main () {
+	// Initializing
 	double sum = 0;  
 	int count = 0;
-	double nr;    
-	cout << "Enter your numbers one at a time and hit Enter.  Terminate with ctrl-z" << endl;
-	while (!cin.eof()) {  
+	double nr;    // user input
+	cout << "Enter your numbers one at a time and hit Enter. Terminate with ctrl-z" << endl;
+	while (!cin.eof()) {  // loop control
 		cin >> nr;
-		if (!cin.eof() && cin.good()) { 
+		if (!cin.eof() && cin.good()) { // test !cin.eof() again in case the user enter EOF in the cin
 			sum += nr; 
-			count++;
+			count++; // only count valid input
 		}
-		else
-			if (!cin.eof() && !cin.good()) {
-				cout << "Invalid Input, Try Again!" << endl;
-				cin.clear();            
-				cin.ignore(256, '\n');  
-			}   
+		else if (!cin.eof() && !cin.good()) {
+			cout << "Invalid Input, Try Again!" << endl;
+			cin.clear(); // clear the cin status bits     
+			cin.ignore(256, '\n');  // clear the input buffer up to the newline character
+		}   
 	}
-	if (count == 0)  
+	// for program termination
+	if (count == 0) { 
 		cout << "No valid data entered." << endl;
-	else
+	}
+	else {
 		cout << "The sum is " << sum << endl
 			 << "The average is " << sum / count << endl;
+	}
 	return EXIT_SUCCESS;
 }
