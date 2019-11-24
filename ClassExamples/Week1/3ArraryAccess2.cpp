33b557579e874e425ddebf53d3ca363e8f5a4625
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-23 19:50:04 -0500

    Update 3ArraryAccess2.cpp

--- ClassExamples/Week1/3ArraryAccess2.cpp
+++ ClassExamples/Week1/3ArraryAccess2.cpp
@@ -1,4 +1,5 @@
-// 
+// create a static array and access the elements in three ways
+// using a combination of array name, integer displacement and indexing
 
 #include <iostream> 
 #include <iomanip>
@@ -10,17 +11,24 @@ int main() {
 	cout << (int)Arr << endl;
 	int i, n;
 	
+	// using array name and an index
 	for (i = 0; i < 10; i++)
 		cout << " " << Array[i];
 	cout << endl;
+	
+	// using indirection with the array name plus an index
 	for (n = 0; n < 10; n++)
 		cout << " " << *(Array + n);
 	cout << endl;
+	
+	// to process an array from an intermediate point to the end 
 	for (n = 5, i = 0; i < 6; i++)
 		cout << " " << (Array + n)[i];
 	cout << endl;
+	
 	for (n = 5, i = 0; i > -10; i--)
 		cout << " " << (Array + n)[i];
 	cout << endl;
+	        system("PAUSE") // in case your system expects a PAUSE before ending
 	return 0;
 }
