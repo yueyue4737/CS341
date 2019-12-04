b083d349facab903ebe34c99fd639ee641902b42
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-04 12:45:05 -0500

    Create 4firstClassMain.cpp

---
+++ ClassExamples/4firstClassMain.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+#include "FirstClass.h"
+int main() {
+	cout << "Welcome to the FirstClassDemo program" << endl;
+	FirstClass A;
+	A.putNr(5);
+	cout << "Value of nr data member  in A object is "
+		<< A.getNr() << endl;
+	return EXIT_SUCCESS;
+}
