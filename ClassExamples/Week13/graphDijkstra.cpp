292883e11c7853ca9753aa0ac4f024899a5a1814
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-18 09:47:09 -0500

    Update and rename graph5Dykstra.cpp to graphDijkstra.cpp

---
+++ ClassExamples/Week13/graphDijkstra.cpp
@@ -0,0 +1,11 @@
+// Dijkstra's Shortest-Distance Algorithm
+// reference1: textbook pp772
+// reference2: program graphDijkstra.cpp
+
+// step1: initialize the distance array
+
+// step2: initialize the set of allowed vertices to be the empty set
+
+// step3: compute the complete distance array
+
+// step4: output the values in the distance array
